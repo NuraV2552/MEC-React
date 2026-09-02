d6ccd97b347260b7cf2c824056dd1b3a13d7352b
Varun <varunvinod.raka@gmail.com>
2026-09-02 17:22:41 +0400

    Progresshalf

--- components/projects-gallery.tsx
+++ components/projects-gallery.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { Button } from "@/components/ui/button"
 
-const categories = ["All", "Residential", "Commercial", "Industrial", "Hospitality"]
+const categories = ["All", "Residential", "Commercial", "Industrial"]
 
 const projects = [
   {
